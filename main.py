ef3073670e8ac1880d738e892f5737fcfd4707dc
Ravin Chowdhury <chowravc@gmail.com>
2023-04-13 10:08:30 -0600

    Some notebooks added

--- main.py
+++ main.py
@@ -18,6 +18,21 @@ from utils import *
 from gauss import *
 from llsfit import *
 
+def splitall(path):
+	allparts = []
+	while 1:
+		parts = os.path.split(path)
+		if parts[0] == path:  # sentinel for absolute paths
+			allparts.insert(0, parts[0])
+			break
+		elif parts[1] == path: # sentinel for relative paths
+			allparts.insert(0, parts[1])
+			break
+		else:
+			path = parts[0]
+			allparts.insert(0, parts[1])
+	return allparts
+
 
 ### Create calibration points of known spectrum
 def calibrationPoints(filePath):
@@ -598,7 +613,7 @@ def createCsv(filePath, caliPath):
 	df = pd.DataFrame(df)
 
 	## Split filepath into individual componenets
-	pathComps = filePath.split(os.sep)
+	pathComps = splitall(filePath)
 
 	## Filename as csv
 	outFile = pathComps[-1].split('.')[-2] + "_calibrated.csv"
